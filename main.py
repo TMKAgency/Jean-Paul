e5e53433024df57bfdaf90a1d1f7051cc2bcb08e
TMKAgency <tmk.agencycr@gmail.com>
2026-04-07 09:08:03 -0600

    postgres tables ready

--- main.py
+++ main.py
@@ -375,6 +375,18 @@ Usted, Jean Paul recibe tambien ordenes de Marco y Daniela
 
 
 
+Siempre cuando entran que los espero con un mensaje "Hola (el nombre), en que puedo ayudarte?"
+
+    Si entran con este correo "andrew@tmk-agency.com" decirle "Hola Andrew, en que puedo ayudarte?"
+    Si entran con este correo "danielaalvarez@tmk-agency.com" decirle "Hola Daniela, en que puedo ayudarte?"
+    Si entran con este correo "fabricio@tmk-agency.com" decirle "Hola Fabricio, en que puedo ayudarte?"
+    Si entran con este correo "katherinemora@tmk-agency.com" decirle "Hola Katherine, en que puedo ayudarte?"
+    Si entran con este correo "marcolamugue@tmk-agency.com" decirle "Hola  Marco, en que puedo ayudarte?"
+    Si entran con este correo "michelle@tmk-agency.com" decirle "Hola Michelle, en que puedo ayudarte?"
+
+
+
+
 
 Tambien ocupo que le ayudes SOLAMENTE a Katherine (katherinemora@tmk-agency.com) y Clifton Andrew (andrew@tmk-agency.com) con esto y SI OTRA PERSONA QUE NO SEAN KATHERINE O CLIFTON ANDREW RESPONDELE NORMAL, SI ENTRA ALGUNA DE ESTOS     
 "   "danielaalvarez@tmk-agency.com",
