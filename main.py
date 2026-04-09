ccc86687ef23e5848c8fca6c0700b1987f4f7d60
TMKAgency <tmk.agencycr@gmail.com>
2026-04-09 08:58:45 -0600

    postgres tables ready

--- main.py
+++ main.py
@@ -378,12 +378,13 @@ Usted, Jean Paul recibe tambien ordenes de Marco y Daniela
 
 Siempre cuando entran que los espero con un mensaje "Hola (el nombre), en que puedo ayudarte?"
 
-Si entran con este correo "andrew@tmk-agency.com"  =  decirle "Hola Andrew, en que puedo ayudarte?"
-Si entran con este correo "danielaalvarez@tmk-agency.com"  =  decirle "Hola Daniela, en que puedo ayudarte?"
-Si entran con este correo "fabricio@tmk-agency.com"  =  decirle "Hola Fabricio, en que puedo ayudarte?"
-Si entran con este correo "katherinemora@tmk-agency.com"  =  decirle "Hola Katherine, en que puedo ayudarte?"
-Si entran con este correo "marcolamugue@tmk-agency.com"  =  decirle "Hola  Marco, en que puedo ayudarte?"
-Si entran con este correo "michelle@tmk-agency.com"  =  decirle "Hola Michelle, en que puedo ayudarte?"
+Si Andrew Clifton entran con este correo "andrew@tmk-agency.com"  =  decirle "Hola Andrew, en que puedo ayudarte?"
+Si Daniela entran con este correo "danielaalvarez@tmk-agency.com"  =  decirle "Hola Daniela, en que puedo ayudarte?"
+Si Fabricio entran con este correo "fabricio@tmk-agency.com"  =  decirle "Hola Fabricio, en que puedo ayudarte?"
+Si Katherine entran con este correo "katherinemora@tmk-agency.com"  =  decirle "Hola Katherine, en que puedo ayudarte?"
+Si Marco entran con este correo "marcolamugue@tmk-agency.com"  =  decirle "Hola  Marco, en que puedo ayudarte?"
+Si Michelle entran con este correo "michelle@tmk-agency.com"  =  decirle "Hola Michelle, en que puedo ayudarte?"
+Si Breyner Steve Lopez entran con este correo "marcolamugue@tmk-agency.com"  =  decirle "Hola  Breyner, en que puedo ayudarte?"
 
 
 
